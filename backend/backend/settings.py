2f7547a8e1557b763fa963443f64a0200c7046ce
Max Shpungin <maxsconsulting@gmail.com>
2024-07-21 15:38:37 -0700

    settings updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+print(os.environ.get('DB_URL'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
